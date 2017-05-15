15c53215163b248612b37a00f99076a095053605
emobossemo <408455428@qq.com>
2017-05-15 08:46:16 +0800

    20170515

--- gpzj/src/api/advisment.js
+++ gpzj/src/api/advisment.js
@@ -16,17 +16,17 @@ export const advertises = {
 	"columnContent":[{
 		"tabValue": "tab1",
 		"lists":[{
-			"url":"/hxwwz/rest/json/gaoshou/info/param/page/hot_detail?t=hot_detail&articleid=2833&channel=newstock_list",
-			"title":"抓热点:<span style='color:#c52728'>弱市下寻找绩优股</span>",
-			"describeText":"<span style='font-size:13px;'>本期精选</span><span style='color:#c52728;font-size:13px;'>2只中低价股，无创业板...</span>",
+			"url":"/hxwwz/rest/json/gaoshou/info/param/page/hot_detail?t=hot_detail&articleid=3022&channel=newstock_list",
+			"title":"抓热点:<span style='color:#c52728'>机会或是跌出来的</span>",
+			"describeText":"<span style='font-size:13px;'>本期精选</span><span style='color:#c52728;font-size:13px;'>2只调整或到位的……</span>",
 			"iconUrl":"http://r0.hx168.com.cn/gpzj/img/v4/icon-hot-1.png",
 			"btnText":"阅读",
 			"primary":true,
 			"secondary":false,
 		},{
-			"url":"/hxwwz/rest/json/gaoshou/info/param/page/article_detail?t=article_detail&articleid=2799&channel=newstock_list",
-			"title":`上期“抓热点”回顾`,
-			"describeText":"<span style='color:#c52728;font-size:13px;'>分众传媒两交易日涨幅超5%！</span>",
+			"url":"/hxwwz/rest/json/gaoshou/info/param/page/article_detail?t=article_detail&articleid=2700&channel=newstock_list",
+			"title":`“<span style='color:#c52728'>抓热点</span>”服务将全面启动`,
+			"describeText":"<span style='font-size:13px;'>一旦发现热点题材，第一时间告知您</span>",
 			"iconUrl":"http://r0.hx168.com.cn/gpzj/img/v4/icon-hot-2.png",
 			"btnText":"查看",
 			"primary":false,
