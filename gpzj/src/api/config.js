dae52f57aa60c32347035ddc10d5f8b524a021fb
emobossemo <408455428@qq.com>
2017-05-16 13:55:42 +0800

    20170516

--- gpzj/src/api/config.js
+++ gpzj/src/api/config.js
@@ -1,12 +1,12 @@
 // wx
-export const filePath = "http://wx.hx168.com.cn/";
-export const wechatToken = "d7f1a00678928a59438b40c32d33025d164c773b";
+// export const filePath = "http://wx.hx168.com.cn/";
+// export const wechatToken = "d7f1a00678928a59438b40c32d33025d164c773b";
 
 
 
 // export const filePath = process.env.NODE_ENV !== 'production' ? "/" : "http://wxtest.hx168.com.cn/";
 // export const filePath = "/";
-// export const filePath = "http://wxtest.hx168.com.cn/";
-// export const wechatToken = "da39a3ee5e6b4b0d3255bfef95601890afd80710";
+export const filePath = "http://wxtest.hx168.com.cn/";
+export const wechatToken = "da39a3ee5e6b4b0d3255bfef95601890afd80710";
 
 
